cd713f925d031d2eb1a3ffef2570c70d158c37a0
DevanshSharmaCL <526473+gb96@users.noreply.github.com>
2026-01-22 17:09:08 +0530

    Rotation and border radius has been added

--- Projects/Random Color notes genrator/RCN.js
+++ Projects/Random Color notes genrator/RCN.js
@@ -4,15 +4,21 @@ let main = document.querySelector('main');
 btn.addEventListener('click', function () {
     let div = document.createElement('div');
 
-    let x = Math.random() * 100;
-    let y = Math.random() * 100;
+    let x = Math.random() * 80;
+    let y = Math.random() * 80;
 
     let r = Math.floor(Math.random() * 255);
     let g = Math.floor(Math.random() * 255);
     let b = Math.floor(Math.random() * 255);
+    let rot = Math.floor(Math.random() * 360);
+    let edge = Math.floor(Math.random() * 50)
+    // let w = Math.floor(Math.random()*200)
+    // let h = Math.floor(Math.random()*200)
 
     div.style.width = '50px';
     div.style.height = '50px';
+    div.style.borderRadius = edge + '%'
+    div.style.rotate = rot + 'deg'
     div.style.backgroundColor = `rgb(${r},${g},${b})`;
 
     div.style.position = 'absolute';
