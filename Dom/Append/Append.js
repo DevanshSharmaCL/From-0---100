2412f2c32470923d8de96faa22bf0c0e509d082f
DevanshSharmaCL <179486114+DevanshSharmaCL@users.noreply.github.com>
2026-01-26 19:40:04 +0530

    This is how we append child into another elements

--- Dom/Append/Append.js
+++ Dom/Append/Append.js
@@ -2,5 +2,11 @@ let btn = document.querySelector('button');
 
 btn.addEventListener('click',()=>{
     let createH1 = document.createElement('h1')
+    createH1.innerHTML = "This is created using DOM"
+    let createDiv = document.createElement('div')
+    let body = document.querySelector('body')
+    body.appendChild(createDiv)
+    createDiv.appendChild(createH1)
+    console.log(createDiv)
     console.log(createH1) 
 })
