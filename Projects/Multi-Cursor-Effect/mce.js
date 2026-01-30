541d32bd459c4ce26584e35b56b294b9e05f5285
DevanshSharmaCL <179486114+DevanshSharmaCL@users.noreply.github.com>
2026-01-30 14:06:04 +0530

    Multi-cursor-effect has been fixed

--- Projects/Multi-Cursor-Effect/mce.js
+++ Projects/Multi-Cursor-Effect/mce.js
@@ -1,20 +1,26 @@
 let div = document.querySelectorAll('.col')
-let img = document.querySelectorAll('img')
 
-console.log(img)
 
-div.forEach(e=>{
-    e.addEventListener('mouseenter',function(){
-        e.style.backgroundColor = '#EF88AD'
-        console.log("moving")
-    })
-    
-    e.addEventListener('mouseleave',function(){
-        e.style.backgroundColor = 'transparent'
-    })
-    
-    e.addEventListener('mousemove',function(){
-        e.style.top = img.x + 'px'
-        e.style.left = img.y + 'px'
-    })
+div.forEach(function(e){
+
+    let img = e.querySelector('img')
+
+
+ e.addEventListener('mouseenter',function(){
+    e.style.backgroundColor = "red"
+    img.style.opacity = '1'
+})
+
+
+e.addEventListener('mouseleave',function(){
+    e.style.backgroundColor = "transparent"
+    img.style.opacity = '0'
+    // console.log("leaved")
+ })
+
+
+ e.addEventListener('mousemove',function(move){
+    img.style.top = move.y + 'px'
+    // img.style.left = move.x + 'px'
+ })
 })
