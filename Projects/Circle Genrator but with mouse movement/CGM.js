a567496400d66d668866c79324ffd7d1661296e2
DevanshSharmaCL <179486114+DevanshSharmaCL@users.noreply.github.com>
2026-02-04 14:03:55 +0530

    New directory has been made also some changes has done in good peoject section

--- Projects/Circle Genrator but with mouse movement/CGM.js
+++ Projects/Circle Genrator but with mouse movement/CGM.js
@@ -5,19 +5,20 @@ document.addEventListener('mousemove', function(e) {
     let r = Math.floor(Math.random()*255)
     let g = Math.floor(Math.random()*255)
     let b = Math.floor(Math.random()*255)
-    let x = Math.random()*94
-    let y = Math.random()*96
+    let y = Math.random()*93
+    let x = Math.random()*97
     let h = Math.floor(Math.random()*(50 - 10)+10)
     let w = Math.floor(Math.random()*(50 - 10)+10)
     let rot = Math.floor(Math.random()*360)
+    let br = Math.floor(Math.random()*50)
 
     div.style.height = h+'px'
     div.style.width = w+'px'
     div.style.rotate = rot + 'deg'
-    div.style.top = x + '%'
-    div.style.left = y + '%'
+    div.style.top = y + '%'
+    div.style.left = x + '%'
     div.style.position = 'absolute'
     div.style.backgroundColor = `rgb(${r},${g},${b})`
-
+    div.style.borderRadius = br + '%'
     main.appendChild(div)
 });
