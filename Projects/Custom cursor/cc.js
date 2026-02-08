80da53be3c384d239c0b940f0107d31d0379afff
DevanshSharmaCL <179486114+DevanshSharmaCL@users.noreply.github.com>
2026-02-08 13:53:00 +0530

    Everything got organized

--- Projects/Custom cursor/cc.js
+++
@@ -1,8 +0,0 @@
-let main = document.querySelector(".main")
-let cursor = document.querySelector(".cursor")
-
-main.addEventListener('mousemove',function(mov){
-
-    cursor.style.left = mov.x + 'px'
-    cursor.style.top = mov.y + 'px'
-})
