88aad302ad09f7a3e0335eb7c24a569c015fa454
DevanshSharmaCL <179486114+DevanshSharmaCL@users.noreply.github.com>
2026-02-08 13:35:27 +0530

    Matrix writing

--- Effects/Matrix effect/script.js
+++ Effects/Matrix effect/script.js
@@ -1,33 +1,26 @@
-let para = document.querySelector('h1')
-
-let character = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-
-let text = para.textContent.trim()
-
+let h1 = document.querySelector('h1')
+let character = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+}{";  // 76 
+let text = h1.innerText
+console.log(character.length)
 let iteration = 0 
 
-para.addEventListener('mouseenter', function(){
-
-    let interval = setInterval(() => {
-
-        let str = text.split('').map((char, index) => {
-
-            if(index < iteration){
-                return char
-            }
-
-            return character[Math.floor(Math.random() * character.length)]
-
-        }).join('')
-
-        para.textContent = str
-
-        iteration += 0.5
-
-        if(iteration >= text.length){
-            clearInterval(interval)
+function matrixText(){
+    const str = text.split("").map((char , index)=>{
+        if(index<iteration){
+            return char
         }
+        return character.split("")[Math.floor(Math.random()*character.length)] 
+    }).join("")
 
-    }, 30)
+    h1.innerText = str
+    iteration += 0.5
+}
+    
+h1.addEventListener('mouseenter', function(e){
+    setInterval(() => {
+        matrixText()
+        
+    }, 30);
 
 })
+
