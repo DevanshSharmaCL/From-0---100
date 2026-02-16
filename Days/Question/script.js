a1a8a0df578c7e801e8e4b550f70765089559956
DevanshSharmaCL <179486114+DevanshSharmaCL@users.noreply.github.com>
2026-02-16 19:19:39 +0530

    Discounted price of the obnjected

--- Days/Question/script.js
+++ Days/Question/script.js
@@ -1,28 +1,8 @@
-// let user = {
-//     name:"cyber",
-//     email: "CyberLi55@gmail.com",
-//     login:function(){
-//         console.log("logged in")
-//     }
-// }
-
-// user.login()
-
-class user {
-    constructor(name, email){
-        this.name = name
-        this.email = email 
+ let product = {
+    name : "pen",
+    price : 100,
+    discountedPrice : function(){
+        return this.price - 10
     }
-    loggedIn(){
-        console.log("LoggeTIN")
-    }
-}   
-
-
-let user1 = new user("cyber" ,"cyber@k.k")
-let user2 = new user("light" ,"light@k.k")
-let user3 = new user("dark" ,"dark@k.k")
-let user4 = new user("beast" ,"beast@k.k")
-let user5 = new user("ckl" ,"ckl@k.k")
-
-console.log(user1,user2,user3,user4,user5)
+}
+console.log(product.discountedPrice())
