868ff6bf4bd101bf0c9dd805192a6ebb54cbb2fe
DevanshSharmaCL <179486114+DevanshSharmaCL@users.noreply.github.com>
2026-02-25 22:17:49 +0530

    Promises

--- Days/0-Practice/script.js
+++ Days/0-Practice/script.js
@@ -1,31 +1,11 @@
-function loginUser(username , cb){
-    console.log("login user.....")
-    setTimeout(() => {
-        cb({
-            id:12121,
-            username:"cyber"
-        })
-    }, 2000);
-}
+let promise = new Promise(function(res,rej){
+    let rn = Math.floor(Math.random()*10);
+    if(rn>5) res("resolved with " + rn)
+        else rej("rejected with " + rn)
+})
 
-function fetchPermission(id , cb){
-    console.log("fetching permission....")
-    setTimeout(() => {
-        cb(["read", "write", "delete"])
-    }, 3000);
-}
-
-function loadDashboard(permission , cb){
-    console.log("loading dashboard")
-    setTimeout(() => {
-        cb()
-    }, 4000);
-}
-
-loginUser("cyber",function(userData){
-    fetchPermission(userData.id,function(permission){
-        loadDashboard(permission,function(){
-            console.log("dashboard loaded")
-        })
-    })
+promise.then(function(val){
+    console.log(val)
+}).catch(function(val){
+    console.log(val)
 })
