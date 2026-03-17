039b0f1fc33265c49416dafbe9492c613b6a3615
DevanshSharmaCL <179486114+DevanshSharmaCL@users.noreply.github.com>
2026-03-17 19:23:57 +0530

    Email sender with timeout

---
+++ Real Practice/Projects/Bulk Email sender/script.js
@@ -0,0 +1,25 @@
+let users = [
+  "nova.stream42@gmail.com",
+  "pixel.shadow17@yahoo.com",
+  "cyberleaf88@outlook.com",
+  "lunar.byte29@gmail.com",
+  "echo.vector61@protonmail.com",
+];
+
+function sendEmail(email) {
+  return new Promise((resolve, reject) => {
+    let time = Math.floor(Math.random() * 5);
+
+    setTimeout(() => {
+      let probability = Math.floor(Math.random() * 10);
+      if ((probability) > 5) resolve(console.log("email sent succesfully", probability));
+      else reject(console.log("email failed", probability));
+    }, time * 1000);
+    console.log(time)
+  });
+}
+
+// now settimout can do 2 things from inside , agar resolve chala toh uska then state chalega , agar reject chala toh uska catch state chalega
+
+
+sendEmail("cyberlight@101")
