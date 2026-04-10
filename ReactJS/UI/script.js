3b465120b0abdc7901d07164804c485fda30402d
DevanshSharmaCL <179486114+DevanshSharmaCL@users.noreply.github.com>
2026-04-10 19:39:33 +0530

    replica

--- ReactJS/UI/script.js
+++ ReactJS/UI/script.js
@@ -1,4 +1,14 @@
-  const head = React.createElement("h1", {className:"heading"}, "this is heading ")
-const root = ReactDOM.createRoot(document.getElementById("root"))
+const app = React.createElement("main",null,
+    React.createElement('section',null,
+        React.createElement('div',{className:"card"},"card",
+            React.createElement('div',{className:"img-container"},"img-cont", 
+                React.createElement('img',null)
+            ),
+            React.createElement('div',null,"this is div"),
+        )
+    )
+) 
 
-  root.render(head)
+const root = ReactDOM.createRoot(document.querySelector(".root"))
+
+root.render(app)
--- vite+reactJS/testing/src/App.jsx
+++ vite+reactJS/testing/src/App.jsx
@@ -1,32 +1,32 @@
-function Card(props){
-  return(
-    <div>
-      <h1>{props.title}</h1>
-      <img src={props.img}/>
-      <button>click me</button>
-    </div>
-  )
-}
-const data = [
-  { title: "React", img: "https://via.placeholder.com/150" },
-  { title: "JavaScript", img: "https://via.placeholder.com/150" },
-  { title: "HTML", img: "https://via.placeholder.com/150" }
-];
+import "./App.css";
+
 
 
 function App() {
   return (
-    <div>
-      <navbar></navbar>
-      <div className="main-section">
-        {
-          data.map((e,index)=>{
-            return <Card key={index} title={e.title} img={e.img}/>
-          })
-        }
-      </div>
+    <div className="body">
+      <section>
 
-      <footer></footer>
+        <div className="card">
+          
+          <div className="top">
+            <div className="top-heading">
+              <h1>head-1</h1>
+              <h1>head-2</h1>
+              <h1>head-3</h1>
+            </div>
+            <div className="content">
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus aspernatur expedita suscipit! Autem ullam officiis eum quibusdam non excepturi dicta delectus, quos ab quis illo atque? Iure nesciunt doloribus aliquam.
+              Ratione, quam aut, accusamus sit commodi fugiat saepe iusto cupiditate eaque veniam architecto officiis tenetur repellat excepturi laudantium ea nulla. Voluptatibus non natus necessitatibus deleniti error obcaecati optio eaque expedita!</p>
+            </div>
+          </div> 
+          <div className="bottom">
+            <div className="left"></div>
+            <div className="right"></div>
+          </div>  
+        </div>
+      </section>
+ 
     </div>
   );
 }
