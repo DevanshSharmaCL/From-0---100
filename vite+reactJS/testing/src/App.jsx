0ebf5f03aeca712c6821f9c50e4bcdc9e14fb4e0
DevanshSharmaCL <179486114+DevanshSharmaCL@users.noreply.github.com>
2026-04-14 16:17:20 +0530

    Passing as react

--- vite+reactJS/testing/src/App.jsx
+++ vite+reactJS/testing/src/App.jsx
@@ -1,34 +1,8 @@
-import "./App.css";
-
-
-
-function App() {
-  return (
-    <div className="body">
-      <section>
-
-        <div className="card">
-          
-          <div className="top">
-            <div className="top-heading">
-              <h1>head-1</h1>
-              <h1>head-2</h1>
-              <h1>head-3</h1>
-            </div>
-            <div className="content">
-              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus aspernatur expedita suscipit! Autem ullam officiis eum quibusdam non excepturi dicta delectus, quos ab quis illo atque? Iure nesciunt doloribus aliquam.
-              Ratione, quam aut, accusamus sit commodi fugiat saepe iusto cupiditate eaque veniam architecto officiis tenetur repellat excepturi laudantium ea nulla. Voluptatibus non natus necessitatibus deleniti error obcaecati optio eaque expedita!</p>
-            </div>
-          </div> 
-          <div className="bottom">
-            <div className="left"></div>
-            <div className="right"></div>
-          </div>  
-        </div>
-      </section>
- 
-    </div>
-  );
+const App = ()=>{
+  return <div>
+    <h1>i am cyber</h1>
+    <p>i am light</p>
+  </div>
 }
 
-export default App;
+export default App
